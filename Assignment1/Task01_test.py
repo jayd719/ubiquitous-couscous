f3aa46e73328f6a54d14ea1344cb716cdd31de70
jd <jashansingh29@gmail.com>
2025-01-30 17:38:06 -0500

    splitForA1T1

---
+++ Assignment1/Task01_test.py
@@ -0,0 +1,20 @@
+"""-------------------------------------------------------
+CP322: Assignment 1- Task One
+-------------------------------------------------------
+Author:  JD
+ID:      91786
+Uses:    numpy
+Version:  1.0.8
+__updated__ = Thu Jan 30 2025
+-------------------------------------------------------
+"""
+
+import os
+import numpy as np
+import statistics
+
+os.system("clear" if os.name == "posix" else "cls")
+
+
+class KNN_Classifier:
+    pass
