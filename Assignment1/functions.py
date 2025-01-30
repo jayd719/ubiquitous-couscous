9ccc87459f10e90da11b5353e178d30c1e94e9d0
jd <jashansingh29@gmail.com>
2025-01-30 17:20:09 -0500

    distanceFunctions

---
+++ Assignment1/functions.py
@@ -0,0 +1,60 @@
+"""-------------------------------------------------------
+CP322: Assignment 1- Custom Functions Library One
+-------------------------------------------------------
+Author:  JD
+ID:      91786
+Uses:    numpy,openCV,matplotlib,pandas,OpenCV
+Version:  1.0.8
+__updated__ = Thu Jan 30 2025
+-------------------------------------------------------
+"""
+
+import numpy as np
+
+import os
+
+
+def calculate_euclidean_distance(p1, p2):
+    """Calculate the Euclidean distance between two points."""
+    assert len(p1) == len(p2), "Dimension mismatch between the two points"
+    dist = 0
+    for i in range(len(p1)):
+        dist += (p1[i] - p2[i]) ** 2
+    return np.sqrt(dist)
+
+
+def calculate_manhattan_distance(p1, p2):
+    assert len(p1) == len(p2), "Dimension mismatch between the two points"
+    dist = 0
+    for i in range(len(p1)):
+        dist += abs(p1[i] - p2[i])
+    return dist
+
+
+if __name__ == "__main__":
+    os.system("clear" if os.name == "posix" else "cls")
+    print("Tesing Distance Functions")
+    print()
+    print("Test Case 1-2D")
+    p1 = (1, 2)
+    p2 = (5, 6)
+    distance = calculate_euclidean_distance(p1, p2)
+    print(f"Euclidean Distance between {p1} and {p2}: {distance:.4f}")
+    distance = calculate_manhattan_distance(p1, p2)
+    print(f"Manhattan Distance between {p1} and {p2}: {distance:.4f}")
+    print()
+    print("Test Case 2-3D")
+    p1 = (1, 2, 3)
+    p2 = (5, 6, 7)
+    distance = calculate_euclidean_distance(p1, p2)
+    print(f"Euclidean Distance between {p1} and {p2}: {distance:.4f}")
+    distance = calculate_manhattan_distance(p1, p2)
+    print(f"Manhattan Distance between {p1} and {p2}: {distance:.4f}")
+    print()
+    print("Test Case 3-4D")
+    p1 = (1, 2, 3, 4)
+    p2 = (5, 6, 7, 8)
+    distance = calculate_euclidean_distance(p1, p2)
+    print(f"Euclidean Distance between {p1} and {p2}: {distance:.4f}")
+    distance = calculate_manhattan_distance(p1, p2)
+    print(f"Manhattan Distance between {p1} and {p2}: {distance:.4f}")
